0734b3e00c7e511cfb94067232c72642b562d6d7
Song <>
2019-10-25 18:50:23 +0800

    添加QSBaseCollectionReusableView

---
+++ QSBaseView/QSBaseView/BaseView/QSBaseCollectionReusableView.swift
@@ -0,0 +1,29 @@
+//
+//  QSBaseCollectionReusableView.swift
+//  BeeBoxClient
+//
+//  Created by Song on 2019/10/24.
+//  Copyright © 2019 Song. All rights reserved.
+//
+
+import UIKit
+
+open class QSBaseCollectionReusableView: UICollectionReusableView, QSBaseViewProtocol {
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        qs_setupSubViews()
+        qs_binding()
+    }
+    
+    required public init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - QSBaseViewProtocol
+    open func qs_setupSubViews() {
+    }
+    
+    open func qs_binding() {
+    }
+}
